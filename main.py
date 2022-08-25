d97dafac5f4f9d771bce8c54c6154d1cab424428
7years <81397905+564725728@users.noreply.github.com>
2022-08-25 12:58:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_humidity():
   weather = res['data']['list'][0]
   return weather['humidity']
 
-  def get_lastUpdateTime():
+ def get_lastUpdateTime():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]

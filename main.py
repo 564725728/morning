eb808302831cac0bc9d7a5a6f1a416761727d3d7
7years <81397905+564725728@users.noreply.github.com>
2022-08-25 13:13:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_lastUpdateTime():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['lastUpdateTime']
+  return weather['date']
 # 到这里结束
 
 def get_count():

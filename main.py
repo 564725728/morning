a965f4103f10b05c2cba49fc02b3012dd0445baa
7years <81397905+564725728@users.noreply.github.com>
2022-08-25 11:40:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_humidity():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['city'])
+  return city['city'])
 # 到这里结束
 
 def get_count():
